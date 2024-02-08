c0449406a3eeb8319abab24b2acfa10dd48557b2
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-02-08 16:55:53 -0300

    Install packages for server

---
+++ src/server/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getPayloadClient } from './get-payload';
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+const start = async () => {
+    const payload = await getPayloadClient()
+
+}
+
+start();
