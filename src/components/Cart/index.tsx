0876b1b96f49576382d622662be5aaa4e87293a1
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-02-07 21:47:07 -0300

    Cart construction

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import { formatPrice } from '@/lib/utils';
 import { ShoppingCart } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
+import { buttonVariants } from '../ui/button';
+import { Separator } from '../ui/separator';
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -12,6 +17,9 @@ import {
 import AddToCart from './AddToCart';
 
 const Cart = () => {
+  const itemCount = 1;
+  const fee = 10;
+
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -29,6 +37,44 @@ const Cart = () => {
         <SheetHeader className="space-y-2.5 pr-6">
           <SheetTitle>Cart (0)</SheetTitle>
         </SheetHeader>
+
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">
+              {/* ToDo Cart */}
+              Cart Items
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+              </div>
+
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link href='/cart' className={buttonVariants({
+                    className: 'w-full'
+                  })}>
+                    Continue to Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div>No Cart</div>
+        )}
       </SheetContent>
     </Sheet>
   );
