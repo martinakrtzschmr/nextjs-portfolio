aa838f7c55fae9091eb638446bc93cbf5d6e7f2d
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-02-11 18:21:36 -0300

    Installed nodemailer, confired email sent

--- src/server/get-payload.ts
+++ src/server/get-payload.ts
@@ -1,12 +1,23 @@
 import dotenv from 'dotenv';
 import path from 'path';
-import payload from 'payload';
+import payload, { Payload } from 'payload';
 import type { InitOptions } from 'payload/config';
+import nodemailer from 'nodemailer';
 
 /* eslint-disable */
 dotenv.config({
   path: path.resolve(__dirname, '../../.env'),
 });
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.resend.com',
+  secure: true,
+  port: 465,
+  auth: {
+    user: 'resend',
+    pass: process.env.RESEND_API_KEY
+  }
+})
 
 let cached = (global as any).payload;
 
@@ -21,7 +32,9 @@ interface Args {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+export const getPayloadClient = async ({
+  initOptions,
+}: Args = {}): Promise<Payload> => {
   if (!process.env.PAYLOAD_SECRET) {
     throw new Error('PAYLOAD_SECRET is missing.');
   }
@@ -32,6 +45,11 @@ export const getPayloadClient = async ({ initOptions }: Args = {}) => {
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: 'onboarding@resend.dev',
+        fromName: 'Digital Portfolio',
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
